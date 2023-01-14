3303c19fe34f7c245cffc14780168ffcf623f1e5
vatsaly <vatsaly2208@gmail.com>
2023-01-14 09:40:18 +0530

    added dummy object to simulate get and post

--- index.js
+++ index.js
@@ -8,9 +8,36 @@ const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get("/",(req , res)=>{
+
+
+let courses = [
+    {
+      id: "11",
+      name: "Learn Reactjs",
+      price: 299,
+    },
+    {
+      id: "22",
+      name: "Learn Angular",
+      price: 399,
+    },
+    {
+      id: "33",
+      name: "Learn Django",
+      price: 499,
+    },
+  ];
+
+  app.get("/",(req , res)=>{
     res.send("<h1> HomePage </h1>")
 })
+  
+  
+  
+  app.get("/api/v1/lco", (req, res) => {
+    res.send("hello from lco docs");
+  });
+  
 
 app.listen(4000 , ()=>{
     console.log("Server is running at port 4000...")
